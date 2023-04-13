2945a8de89f8668258b4daa8b6e9d03c95d83f4b
Shreya-sriv <shreyas.srivastava2000@gmail.com>
2023-04-13 19:13:53 +0530

    working DAY-1

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+class main{
+
+public static void main(String args[])
+{
+    long a,i=0,temp,len;
+    String str;
+    char x;
+    while(true)
+    {
+       i=0,pass=0;
+       
+    }
+}
+}
---
+++ temp.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class temp {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("hdsgchjvfdjhcgdhcbnfbv");
+        int a=sc.nextInt();
+        if(a==1)
+        {
+            System.out.print("\033[H\033[2J");  
+            System.out.flush(); 
+            System.out.println("hii i m shreya"); 
+        }
+        else if(a==2)
+        {
+            System.out.println("mnbvcxxfgkmm");
+        }
+
+    }
+}
